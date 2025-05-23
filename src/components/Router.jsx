b43d61be9a2c80bfec8d78795e9d60f73fbefb6f
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-05-23 15:55:31 +0100

    Initial commit with Create React App Structure

---
+++ src/components/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Property from '../pages/Property';
+import Error from '../pages/Error';
+
+function AppRouter() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/property/:id" element={<Property />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
---
+++ src/pages/Property.jsx
@@ -0,0 +1,4 @@
+function Property() {
+    return <h1>Property Page</h1>;
+}
+export default Property;

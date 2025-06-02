83edda529ed00f186cf9af0e74f2aa719e642cf9
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-06-02 18:06:20 +0100

    creating and initial styling of error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,15 @@
+import React from 'react';
+import '../styles/Error.scss';
+import { Link } from 'react-router-dom';
+
 function Error() {
-    return <h1>Error</h1>;
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <p>Oops! The page you are looking for does not exist.</p>
+            <Link to="/">Return to Home</Link>
+        </div>
+    );
 }
+
 export default Error;

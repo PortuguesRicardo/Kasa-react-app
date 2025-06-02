d9ff3ed5f3f2eb3823938a97f59cadcb5704cdb2
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-06-02 13:33:06 +0100

    removing duplicate Browser Router being aliased as Router.

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';  // removing duplicate BrowserRouter being aliased as Router. Problem with wrapping Browser Router twice
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Property from '../pages/Property';
@@ -6,14 +6,14 @@ import Error from '../pages/Error';
 
 function AppRouter() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/property/:id" element={<Property />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-        </Router>
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/property/:id" element={<Property />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
+
     );
 }
 

ae74d89303c5761ab409c2d3a569c52b53834666
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-06-11 15:27:41 +0100

    adding family-font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/Router';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './index.scss';
+
 function App() {
   return (
     <div className="App">
